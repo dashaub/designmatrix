bf1c36449862c6176d06d069f5c54be0856b708f
david <david@Elbert.SevenSummits>
2015-09-19 16:36:57 -0600

    new file:   .Rbuildignore 	new file:   .Rhistory 	new file:   .Rproj.user/A941DEFD/cpp-complilation-config 	new file:   .Rproj.user/A941DEFD/cpp-definition-cache 	new file:   .Rproj.user/A941DEFD/pcs/debug-breakpoints.pper 	new file:   .Rproj.user/A941DEFD/pcs/files-pane.pper 	new file:   .Rproj.user/A941DEFD/pcs/source-pane.pper 	new file:   .Rproj.user/A941DEFD/pcs/windowlayoutstate.pper 	new file:   .Rproj.user/A941DEFD/pcs/workbench-pane.pper 	new file:   .Rproj.user/A941DEFD/persistent-state 	new file:   .Rproj.user/A941DEFD/saved_source_markers 	new file:   .Rproj.user/A941DEFD/sdb/prop/28C20593 	new file:   .Rproj.user/A941DEFD/sdb/prop/340CCB42 	new file:   .Rproj.user/A941DEFD/sdb/prop/494ECB31 	new file:   .Rproj.user/A941DEFD/sdb/prop/A5AFCCF1 	new file:   .Rproj.user/A941DEFD/sdb/prop/C86D896D 	new file:   .Rproj.user/A941DEFD/sdb/prop/INDEX 	new file:   .Rproj.user/A941DEFD/sdb/s-9DA58BED/A7E885F5 	new file:   .Rproj.user/A941DEFD/sdb/s-9DA58BED/BE004587 	new file:   .Rproj.user/A941DEFD/sdb/s-9DA58BED/CFC88AD4 	new file:   .Rproj.user/A941DEFD/sdb/s-9DA58BED/FB78090B 	new file:   .Rproj.user/A941DEFD/sdb/s-9DA58BED/lock_file 	new file:   DESCRIPTION 	new file:   NAMESPACE 	new file:   R/RcppExports.R 	new file:   R/deleteMeWhenDone.R 	new file:   R/designMatrix.R 	new file:   R/hello.R 	new file:   designmatrix.Rproj 	new file:   man/designMatrix.Rd 	new file:   man/hello.Rd 	new file:   man/rcpp_hello.Rd 	new file:   src/RcppExports.cpp 	new file:   src/RcppExports.o 	new file:   src/designmatrix.so 	new file:   src/rcpp_hello.cpp 	new file:   src/rcpp_hello.o

---
+++ R/deleteMeWhenDone.R
@@ -0,0 +1,19 @@
+# library(lubridate)
+# alldates <- seq(from = as.Date("2000-01-01"), to = as.Date("2020-12-31"), by = "days")
+#
+#
+# for(currdate in seq_along(alldates)){
+#   cat(alldates[currdate])
+# }
+#
+#
+#
+#
+# myfun <- function(x, y){
+#
+#   print(paste0("This is x: ", x))
+#   print(paste0("This is y: ", y))
+#   match.arg(x)
+#   return(match.call())
+#
+# }
