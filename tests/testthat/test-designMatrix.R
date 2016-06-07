40cf05d82c386046bf5299411e50c69753e929ae
David Shaub <dashaub@users.noreply.github.com>
2016-06-07 08:38:10 -0600

    fix typo

--- tests/testthat/test-designMatrix.R
+++ tests/testthat/test-designMatrix.R
@@ -1,6 +1,7 @@
+# Unit tests for the designMatrix() function
 if(require(testthat)){
   context("Testing the deisgnMatrix() function")
-  testthat("Testing for errors", {
+  test_that("Testing for errors", {
     sampleDates <- seq(from = as.Date("1899-01-01"),
                        to = as.Date("2001-01-01"),
                        by = "days")
