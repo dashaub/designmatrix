0e3626c510ac68ce4192c19f5135e7758752f5b7
David Shaub <dashaub@users.noreply.github.com>
2016-06-20 12:23:43 -0600

    more unit tests, separate valid from invalid inputs

--- tests/testthat/test-designMatrix.R
+++ tests/testthat/test-designMatrix.R
@@ -1,12 +1,25 @@
 # Unit tests for the designMatrix() function
 if(require(testthat)){
-  context("Testing the deisgnMatrix() function")
-  test_that("Testing for errors", {
+  context("Testing the designMatrix() function")
+  test_that("Testing valid inputs", {
     sampleDates <- seq(from = as.Date("1899-01-01"),
                        to = as.Date("2001-01-01"),
                        by = "days")
     expect_error(designMatrix(sampleDates, weekdays = "all"), NA)
     #expect_error(designMatrix(sampleDates, weekdays = "monday"), NA)
     expect_error(designMatrix(sampleDates, years = c(1900, 1950, 2000)), NA)
+  })
+  context("Testing the designMatrix() function with invalid syntax")
+  test_that("Testing invalid inputs", {
+    sampleDates <- seq(from = as.Date("1899-01-01"),
+                   to = as.Date("2001-01-01"),
+                   by = "days")
+    expect_error(designMatrix(sampleDates, months = 1:13))
+    expect_error(designMatrix(sampleDates, months = c("February", 3))
+    expect_error(designMatrix(sampleDates, quarters = 1:5))
+    expect_error(designMatrix(sampleDates, quarters = c("Q1", 3))
+    expect_error(designMatrix())
+    expect_error(designMatrix("January 13 2014", days = "all"))
+    
   })
 }
